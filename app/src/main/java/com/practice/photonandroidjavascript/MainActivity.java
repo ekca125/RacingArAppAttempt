76028de22731294114c4e175495ab5701774b7a2
ekca125 <63381869+ekca125@users.noreply.github.com>
2021-09-25 15:30:24 +0900

    practice1

--- app/src/main/java/com/practice/photonandroidjavascript/MainActivity.java
+++ app/src/main/java/com/practice/photonandroidjavascript/MainActivity.java
@@ -3,6 +3,9 @@ package com.practice.photonandroidjavascript;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
+
+import app.cash.quickjs.QuickJs;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +13,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        try (QuickJs engine = QuickJs.create()) {
+            Log.d("Greeting", engine.evaluate("'hello world'.toUpperCase();").toString());
+        }
+
     }
 }
