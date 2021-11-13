2c45d82b1bd82ab93482d1aa6ed7451a3ecbd22b
ekca125 <63381869+ekca125@users.noreply.github.com>
2021-11-13 22:16:18 +0900

    Add SyncFusion

--- RacingArApp/RacingArApp/App.xaml.cs
+++ RacingArApp/RacingArApp/App.xaml.cs
@@ -1,16 +1,24 @@
-﻿using System;
+using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
+[assembly: ExportFont("Montserrat-Bold.ttf",Alias="Montserrat-Bold")]
+     [assembly: ExportFont("Montserrat-Medium.ttf", Alias = "Montserrat-Medium")]
+     [assembly: ExportFont("Montserrat-Regular.ttf", Alias = "Montserrat-Regular")]
+     [assembly: ExportFont("Montserrat-SemiBold.ttf", Alias = "Montserrat-SemiBold")]
+     [assembly: ExportFont("UIFontIcons.ttf", Alias = "FontIcons")]
 namespace RacingArApp
 {
     public partial class App : Application
     {
+        public static string ImageServerPath { get; } = "https://cdn.syncfusion.com/essential-ui-kit-for-xamarin.forms/common/uikitimages/";
         public App()
         {
+
             InitializeComponent();
 
-            MainPage = new MainPage();
+            //MainPage = new MainPage();
+            MainPage = new Views.ArticleCardPage();
         }
 
         protected override void OnStart()
---
+++ RacingArApp/RacingArApp/Views/ArticleCardPage.xaml.cs
@@ -0,0 +1,23 @@
+﻿using RacingArApp.ViewModels;
+using Xamarin.Forms.Internals;
+using Xamarin.Forms.Xaml;
+
+namespace RacingArApp.Views
+{
+    /// <summary>
+    /// Page to display articles as a card type.
+    /// </summary>
+    [Preserve(AllMembers = true)]
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class ArticleCardPage
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="ArticleCardPage" /> class.
+        /// </summary>
+        public ArticleCardPage()
+        {
+            this.InitializeComponent();
+            this.BindingContext = ArticleCardPageViewModel.BindingContext;
+        }
+    }
+}
