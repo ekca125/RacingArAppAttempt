ae25d48207734c0ce1dd597e40891c8973ff3d33
ekca125 <63381869+ekca125@users.noreply.github.com>
2021-11-23 21:36:22 +0900

    Update login

--- app/src/main/java/net/ekcapaper/racingar/activity/LoginActivity.java
+++ app/src/main/java/net/ekcapaper/racingar/activity/LoginActivity.java
@@ -1,11 +1,14 @@
 package net.ekcapaper.racingar.activity;
 
 import android.os.Bundle;
+import android.view.View;
 
+import com.balysv.materialripple.MaterialRippleLayout;
 import com.google.android.material.textfield.TextInputEditText;
 
 import net.ekcapaper.racingar.components.R;
 import net.ekcapaper.racingar.components.activity.login.LoginCardLight;
+import net.ekcapaper.racingar.components.data.ThisApplication;
 
 import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
@@ -16,7 +19,7 @@ public class LoginActivity extends LoginCardLight {
 
     TextInputEditText loginInputEmail;
     TextInputEditText loginInputPassword;
-
+    MaterialRippleLayout loginLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +27,19 @@ public class LoginActivity extends LoginCardLight {
 
         this.loginInputEmail = findViewById(R.id.login_input_email);
         this.loginInputPassword = findViewById(R.id.login_input_password);
+        this.loginLayout = findViewById(R.id.login_layout);
+        this.loginLayout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                executorService.execute(()->{
+                    String email = loginInputEmail.toString();
+                    String password = loginInputPassword.toString();
 
+                    ThisApplication thisApplication = (ThisApplication) (LoginActivity.this.getApplication());
+                    boolean result = thisApplication.getGameAppOperator().authEmail(email,password);
+                    
+                });
+            }
+        });
     }
 }
