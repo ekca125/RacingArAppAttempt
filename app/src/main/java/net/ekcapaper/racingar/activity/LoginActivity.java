0016560f818e562b95cbb839a2b927266b5112d6
ekca125 <63381869+ekca125@users.noreply.github.com>
2021-11-23 21:30:50 +0900

    Update LoginActivity

--- app/src/main/java/net/ekcapaper/racingar/activity/LoginActivity.java
+++ app/src/main/java/net/ekcapaper/racingar/activity/LoginActivity.java
@@ -2,6 +2,9 @@ package net.ekcapaper.racingar.activity;
 
 import android.os.Bundle;
 
+import com.google.android.material.textfield.TextInputEditText;
+
+import net.ekcapaper.racingar.components.R;
 import net.ekcapaper.racingar.components.activity.login.LoginCardLight;
 
 import java.util.concurrent.Executor;
@@ -11,9 +14,16 @@ import java.util.concurrent.Executors;
 public class LoginActivity extends LoginCardLight {
     ExecutorService executorService;
 
+    TextInputEditText loginInputEmail;
+    TextInputEditText loginInputPassword;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.executorService = Executors.newSingleThreadExecutor();
+
+        this.loginInputEmail = findViewById(R.id.login_input_email);
+        this.loginInputPassword = findViewById(R.id.login_input_password);
+
     }
 }
