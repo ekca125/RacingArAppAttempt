f133af937154136e40834adcbd6e2add3d1c0c7c
ekca125 <63381869+ekca125@users.noreply.github.com>
2021-11-23 21:22:32 +0900

    Update LoginActivity.java

--- app/src/main/java/net/ekcapaper/racingar/activity/LoginActivity.java
+++ app/src/main/java/net/ekcapaper/racingar/activity/LoginActivity.java
@@ -9,10 +9,11 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class LoginActivity extends LoginCardLight {
+    ExecutorService executorService;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        ExecutorService executor = Executors.newCachedThreadPool();
-        // use runonuithread
+        this.executorService = Executors.newSingleThreadExecutor();
     }
 }
