cae45caa7a8f2fbd69a0d559d984296605eb8ba5
ekca125 <63381869+ekca125@users.noreply.github.com>
2021-11-24 17:08:14 +0900

    Update CardviewGameMode

--- app/src/main/java/net/ekcapaper/racingar/activity/GameSelectionActivity.java
+++ app/src/main/java/net/ekcapaper/racingar/activity/GameSelectionActivity.java
@@ -1,5 +1,13 @@
 package net.ekcapaper.racingar.activity;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import androidx.cardview.widget.CardView;
+
+import net.ekcapaper.racingar.components.R;
 import net.ekcapaper.racingar.components.activity.bottomnavigation.BottomNavigationBasic;
 import net.ekcapaper.racingar.components.activity.button.ButtonBasic;
 import net.ekcapaper.racingar.components.activity.card.CardBasic;
@@ -7,6 +15,17 @@ import net.ekcapaper.racingar.components.activity.list.ListNewsCard;
 import net.ekcapaper.racingar.components.activity.tabs.TabsBasic;
 
 public class GameSelectionActivity extends CardBasic {
+    CardView cardViewGameModeSingle;
 
-
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        cardViewGameModeSingle = findViewById(R.id.card_view_game_mode_single);
+        cardViewGameModeSingle.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(GameSelectionActivity.this,"test",Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
 }
