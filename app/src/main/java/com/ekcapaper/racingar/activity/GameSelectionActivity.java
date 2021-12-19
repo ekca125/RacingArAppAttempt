f9337e39203aa8a026d9baae77e2996a587462c9
ekca125 <63381869+ekca125@users.noreply.github.com>
2021-12-19 11:54:12 +0900

    Add GameSelectionActivity

---
+++ app/src/main/java/com/ekcapaper/racingar/activity/GameSelectionActivity.java
@@ -0,0 +1,14 @@
+package com.ekcapaper.racingar.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GameSelectionActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_selection);
+    }
+}
