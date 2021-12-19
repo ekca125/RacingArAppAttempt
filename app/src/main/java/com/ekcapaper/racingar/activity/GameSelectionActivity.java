1479e73e059945b5df701db28c50e14586a07f7a
ekca125 <63381869+ekca125@users.noreply.github.com>
2021-12-19 12:02:44 +0900

    Update GameSelectionActivity

--- app/src/main/java/com/ekcapaper/racingar/activity/GameSelectionActivity.java
+++ app/src/main/java/com/ekcapaper/racingar/activity/GameSelectionActivity.java
@@ -3,14 +3,19 @@ package com.ekcapaper.racingar.activity;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.material.components.R;
 
 public class GameSelectionActivity extends AppCompatActivity {
+    Button button_start_single_game;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_game_selection);
+
+        button_start_single_game = findViewById(R.id.button_start_single_game);
+
     }
 }
