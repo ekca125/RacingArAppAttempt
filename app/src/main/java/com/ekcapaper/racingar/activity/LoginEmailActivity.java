5007a5c69ad1883cf18d1779558d05ac6a5e1399
ekca125 <63381869+ekca125@users.noreply.github.com>
2021-12-19 11:32:57 +0900

    Update LoginActivity

--- app/src/main/java/com/ekcapaper/racingar/activity/LoginEmailActivity.java
+++ app/src/main/java/com/ekcapaper/racingar/activity/LoginEmailActivity.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.material.components.R;
+
 public class LoginEmailActivity extends AppCompatActivity {
 
     @Override
