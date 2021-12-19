ab56bcbd1e150fe29c825357ecfaa115cba5554a
ekca125 <63381869+ekca125@users.noreply.github.com>
2021-12-19 18:57:15 +0900

    Update MeterToLongitudeConverter.java

--- app/src/main/java/com/ekcapaper/racingar/maptool/MeterToLongitudeConverter.java
+++ app/src/main/java/com/ekcapaper/racingar/maptool/MeterToLongitudeConverter.java
@@ -1,6 +1,6 @@
 package com.ekcapaper.racingar.maptool;
 
-public class MeterToLongitudeConverter {// 근사값
+public class MeterToLongitudeConverter extends MeterConverter{// 근사값
     // 위도 1분 = 111.32 km
     // 경도 1분 = 40075 km * cos( latitude ) / 360
 
@@ -10,5 +10,19 @@ public class MeterToLongitudeConverter {// 근사값
 
     // 위도 1초 = 0.0309 km = 30m
     // 경도 1초 = 0.024km = 24m
+    final private double longitudeOneMinuteKiloMeter;
+    final private double longitudeOneMinuteMeter;
+    final private double longitudeOneSecondKiloMeter;
+    final private double longitudeOneSecondMeter;
 
+    public MeterToLongitudeConverter(double latitude){
+        longitudeOneMinuteKiloMeter = 40075 * Math.cos(latitude) / 360;
+        longitudeOneMinuteMeter = convertKilometerToMeter(longitudeOneMinuteKiloMeter);
+        longitudeOneSecondKiloMeter = longitudeOneMinuteKiloMeter / 3600;
+        longitudeOneSecondMeter = convertKilometerToMeter(longitudeOneSecondKiloMeter);
+    }
+
+    public double convertMeterToLongitude(double meter){
+        return meter * longitudeOneSecondMeter;
+    }
 }
