2a6e3dd5e0f2d69532926437323472e2db01c647
ekca125 <63381869+ekca125@users.noreply.github.com>
2021-12-25 08:37:07 +0900

    Delete SingleGameActivities

--- app/src/main/java/com/ekcapaper/racingar/activity/GameSelectionActivity.java
+++ app/src/main/java/com/ekcapaper/racingar/activity/GameSelectionActivity.java
@@ -2,12 +2,10 @@ package com.ekcapaper.racingar.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import com.ekcapaper.racingar.activity.single.SingleGamePrepareActivity;
 import com.ekcapaper.racingar.kit.R;
 
 public class GameSelectionActivity extends AppCompatActivity {
@@ -22,8 +20,7 @@ public class GameSelectionActivity extends AppCompatActivity {
         button_start_single_game.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(GameSelectionActivity.this, SingleGamePrepareActivity.class);
-                startActivity(intent);
+                
             }
         });
     }
